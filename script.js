462bb6505ece445ab97f665de7216165d4193bcb
Ossama-E <ossama.elhe@gmail.com>
2023-05-25 12:43:13 -0400

    to deploy

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
 };
 
 const account2 = {
@@ -16,12 +26,32 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
 };
 const account3 = {
   owner: "Alphonso Davies",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
 };
 
 const account4 = {
@@ -29,6 +59,16 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
 };
 
 const account5 = {
@@ -36,6 +76,16 @@ const account5 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 3333,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
 };
 
 const account6 = {
@@ -109,6 +159,8 @@ const inputClosePin = document.querySelector(".form__input--pin");
 const popup = document.querySelector(".popup");
 
 let loggedAccount;
+let sort = false;
+
 // Currency mapping
 const currencies = new Map([
   ["USD", "United States dollar"],
@@ -116,8 +168,22 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
+// Temporary login
+loggedAccount = account1;
+updateAccount(loggedAccount);
+containerApp.style.opacity = 100;
+
+let currDate = new Date();
+let min = `${currDate.getMinutes()}`.padStart(2, 0);
+let hour = currDate.getHours();
+let day = `${currDate.getDate()}`.padStart(2, 0);
+let month = `${currDate.getMonth() + 1}`.padStart(2, 0);
+let year = currDate.getFullYear();
+
+let dateDisplay = `${day}/${month}/${year}, ${hour}:${min}`;
+labelDate.textContent = dateDisplay;
+
 // Popup management
-let sort = false;
 function displayPopup(message) {
   // const popup = document.querySelector(".popup");
   popup.textContent = message;
@@ -152,16 +218,25 @@ function displayMovements(account, sorted = false) {
     : account.movements;
 
   movsToUse.forEach(function (amount, index) {
-    const type = amount > 0 ? `deposit` : `withdrawal`;
+    let currDate = new Date(account.movementsDates[index]);
+    let day = `${currDate.getDate()}`.padStart(2, "0");
+    let month = `${currDate.getMonth() + 1}`.padStart(2, "0");
+    let year = currDate.getFullYear();
+
+    let recordDate = `${day}/${month}/${year}`;
+
+    const type = amount > 0 ? "deposit" : "withdrawal";
     const newMovementHTML = `<div class="movements__row">
     <div class="movements__type movements__type--${type}">${
       index + 1
     }: ${type}</div>
+    <div class="movements__date">${recordDate}</div>
     <div class="movements__value">${amount.toFixed(2)} €</div>
   </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", newMovementHTML);
   });
 }
+
 function displayBalance(account) {
   account.balance = account.movements.reduce((acc, curr) => acc + curr, 0);
   labelBalance.textContent = `${account.balance.toFixed(2)}€`;
@@ -192,6 +267,7 @@ function updateAccount(account, sort = false) {
   calcAccountSummary(account);
   displayBalance(account);
   sort ? displayMovements(account, true) : displayMovements(account);
+  alternateColor();
 }
 function alternateColor() {
   document.querySelectorAll(".movements__row").forEach(function (row, index) {
@@ -255,8 +331,6 @@ btnTransfer.addEventListener("click", function (e) {
     (acc) => acc.username === inputTransferTo.value
   );
 
-  // console.log(validTo);
-
   // Check validity of username
   if (!validTo) {
     displayPopup("Please enter a valid username");
@@ -271,11 +345,16 @@ btnTransfer.addEventListener("click", function (e) {
       // Check if the account holder has enough money
       if (loggedAccount.balance >= +inputTransferAmount.value) {
         // Process the transfer on both ends
-
         loggedAccount.movements.push(-Number(inputTransferAmount.value));
         validTo.movements.push(+inputTransferAmount.value);
+
+        // Display completion
         displayPopup("Transfer Completed!");
         closePopup();
+        // Add transfer date
+        loggedAccount.movementsDates.push(new Date().toISOString());
+        validTo.movementsDates.push(new Date().toISOString());
+        // Update the interface
         updateAccount(loggedAccount);
       } else {
         // User doesn't have enough money
@@ -332,6 +411,7 @@ btnLoan.addEventListener("click", function (e) {
   ) {
     displayPopup("Loan Successfully Deposited");
     closePopup();
+    loggedAccount.movementsDates.push(new Date().toISOString());
     loggedAccount.movements.push(loanAmount);
     updateAccount(loggedAccount);
   } else {
